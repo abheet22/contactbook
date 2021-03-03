38cd11a32621fb513cad059c88338b2a4e8a7c3f
abheet.g <abheet.gupta@ril.com>
2021-03-03 21:20:21 +0530

    updated: delete test cases

--- contacts/tests/test_delete_contacts.py
+++ contacts/tests/test_delete_contacts.py
@@ -69,7 +69,6 @@ class DeleteContactTestCase(TestCase):
         self.assertEqual(response["message"],
                          "Contact id {} is not present in system".format("f967a846-ae13-447a-8e4d-d8fd3cb6b8a7"))
 
-
     def test_delete_contact_data_phone_number_id_invalid(self):
         create_response = self.create_contact_data()
         self.endpoint = self.endpoint.format(id=create_response["payload"]["id"])+\
@@ -85,5 +84,35 @@ class DeleteContactTestCase(TestCase):
         self.assertEqual(response["status_code"], 404)
         self.assertEqual(response["error"],
                          "Unable to perform delete operation due to invalid id")
+
+    def test_delete_contact_data_phone_number_id_valid(self):
+        create_response = self.create_contact_data()
+        extra_params = "?flag_delete_number=true&id={}".format(",".join(create_response["payload"]["nested_ids"]))
+        self.endpoint = self.endpoint.format(id=create_response["payload"]["id"])+extra_params
+        response = call_endpoint_for_microservice(
+            testclient=self.client,
+            endpoint=self.endpoint,
+            req_data=None,
+            username=self.username,
+            password=self.password,
+            method=self.method
+        )
+        self.assertEqual(response["status_code"], 200)
+
+    def test_delete_contact_data_phone_number_id_missing_invalid(self):
+        create_response = self.create_contact_data()
+        extra_params = "?flag_delete_number=true"
+        self.endpoint = self.endpoint.format(id=create_response["payload"]["id"])+extra_params
+        response = call_endpoint_for_microservice(
+            testclient=self.client,
+            endpoint=self.endpoint,
+            req_data=None,
+            username=self.username,
+            password=self.password,
+            method=self.method
+        )
+        self.assertEqual(response["status_code"], 404)
+        self.assertEqual(response["error"],
+                         "id is missing for performing delete operation")
 
 
