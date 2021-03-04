a42c0036fc6258f190d4a48245ae632178921405
abheet.g <abheet.gupta@ril.com>
2021-03-04 12:36:31 +0530

    updated: settings file

--- contactbook/settings.py
+++ contactbook/settings.py
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'contactbook.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "--name--",
+        'USER': "--user--",
+        'PASSWORD': "--password--"
     }
 }
 
